61d94edca3b3aeb8fed613a347c6618a8e7d03c5
Omargaly Bitebayev <omargaly.rs@gmail.com>
2023-09-22 19:49:04 +0600

    Added vue-router, created router for experimental check it works or not, and added TailwindCSS (also checked for working)

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,7 +4,12 @@
 
 <template>
 <div>
-    App
+    <h1 class="text-2xl font-bold">
+        Hello world!
+    </h1>
+    <router-link to="Index">Index</router-link>
+    <router-link to="Page">Page</router-link>
+    <router-view></router-view>
 </div>
 </template>
 
---
+++ resources/js/views/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+<div>
+    Index
+</div>
+</template>
+
+<style scoped>
+
+</style>
